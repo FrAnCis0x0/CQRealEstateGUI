4d62923cc3cef316f72356ff74f24ceca6f320cc
Francis <francis.renzaho@cqumail.com>
2023-05-18 01:19:04 +1000

    makes mainBorderPane available to other classes

---
+++ src/main/java/cqrealestatepackage/model/BorderPaneInfo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cqrealestatepackage.model;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ * @author renza
+ */
+public class BorderPaneInfo {
+    public static BorderPane borderPane;
+}
