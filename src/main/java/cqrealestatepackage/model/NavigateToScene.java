5740d7cc0aefce0eda59cadcb94c3f98798e27ba
Francis <francis.renzaho@cqumail.com>
2023-05-17 18:14:41 +1000

    Added a navigation class that returns a pane

---
+++ src/main/java/cqrealestatepackage/model/NavigateToScene.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cqrealestatepackage.model;
+
+import cqrealestatepackage.App;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author renza
+ */
+public class NavigateToScene {
+    private Pane view;
+    
+    public Pane getFxml(String name){
+        try{
+            FXMLLoader loader = new FXMLLoader(App.class.getResource("view/"+name+".fxml"));
+            view = loader.load();
+        }
+        catch(Exception e){
+            System.out.println("File "+name+" not found ");
+        }
+        
+        return view;
+    }
+}
