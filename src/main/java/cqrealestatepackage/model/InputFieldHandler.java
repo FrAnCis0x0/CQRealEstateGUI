37d6e197cfe94c7ab2a301b8987d2dec013204b3
Francis <francis.renzaho@cqumail.com>
2023-05-20 03:01:31 +1000

    added data validation functions

--- src/main/java/cqrealestatepackage/model/InputFieldHandler.java
+++ src/main/java/cqrealestatepackage/model/InputFieldHandler.java
@@ -7,6 +7,7 @@ package cqrealestatepackage.model;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
@@ -17,9 +18,11 @@ import javafx.scene.control.TextField;
 public class InputFieldHandler {
     private ArrayList<TextField> tfArray;//Stores textField objects
     private ArrayList<TextField> emptyTextField; //stores input fields that still reqquire data
+    private ComboBox[] comboList;
     public InputFieldHandler(){
         this.tfArray = new ArrayList<>();
         this.emptyTextField = new ArrayList<>();
+        comboList = new ComboBox[2];
     }
     //Adds one textfield object to tfArray
     public void addTextField(TextField tf){
@@ -41,6 +44,16 @@ public class InputFieldHandler {
         return (emptyTextField.isEmpty());
     }
     
+    public boolean choiceSelected(ComboBox... cb){
+        for(ComboBox comboBox : cb){
+            if(comboBox.getValue() == null){
+                changeBorderColor(comboBox);
+                return false;
+            }
+        }
+        return true;
+    }
+    
     //checks if text field has only strings
     public boolean tfIsString(TextField tf){
         return tf.getText().matches("[a-zA-Z]+");
@@ -48,6 +61,17 @@ public class InputFieldHandler {
     //checks if text field has only numbers
     public boolean tfIsInteger(TextField tf){
         return tf.getText().matches("[0-9]+");
+    }
+    
+    public boolean numberNumberNotTooBig(TextField tf){
+        try{
+            int temp = Integer.parseInt(tf.getText());
+            return true;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Number is too big");
+            return false;
+        }
     }
     
     //makes sure inputs that require integer have integers
@@ -68,8 +92,25 @@ public class InputFieldHandler {
     
     //validates Date Picker data
     public boolean isDateValid(DatePicker dp){
-        LocalDate selectedDate = dp.getValue();
-        return !(selectedDate == null);
+        //make sure date is not null and the selected date is not a past date
+        if(dp.getValue() != null && validateDate(dp.getValue())){
+            return true;
+        }else{
+            //date selected is in the past
+            if(dp.getValue() != null && !validateDate(dp.getValue())){
+                System.out.println("Show tooltip");
+            }
+            
+            System.out.println("Invalid date.. Color change");
+            return false;
+            
+        }
+
+    }
+    //
+    private boolean validateDate(LocalDate date) {
+        LocalDate currentDate = LocalDate.now();
+        return !date.isBefore(currentDate);
     }
     
     //
@@ -81,9 +122,21 @@ public class InputFieldHandler {
     }
     
     public void changeBorderColor(TextField tf){
+        tf.getStyleClass().remove("textInputFields:focused");
         tf.getStyleClass().add("invalidInput");
+        
         System.out.println("Change Input Color ERROR - Emp");
     }
+    
+    
+    public void changeBorderColor(ComboBox cb){
+        cb.getStyleClass().remove("textInputFields");
+        cb.getStyleClass().add("invalidInput");
+        
+        System.out.println("Change Input Color ERROR - Emp");
+    }
+    
+    
    
     //clears data from all Text Fields in tfArray
     public void clear(){
