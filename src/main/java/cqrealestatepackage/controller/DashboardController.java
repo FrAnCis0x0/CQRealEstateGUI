78d043a5f461b76587799eb6b00e8a5213506abe
Francis <francis.renzaho@cqumail.com>
2023-05-20 02:55:57 +1000

    tableView is functional

--- src/main/java/cqrealestatepackage/controller/DashboardController.java
+++ src/main/java/cqrealestatepackage/controller/DashboardController.java
@@ -5,13 +5,20 @@
 package cqrealestatepackage.controller;
 
 import cqrealestatepackage.App;
+import cqrealestatepackage.model.Land;
+import cqrealestatepackage.model.Sale;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.chart.BarChart;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 /**
  * FXML Controller class
  *
@@ -22,14 +29,42 @@ public class DashboardController implements Initializable {
 
     @FXML
     private BarChart<?, ?> dashboardBarChart;
+    
     @FXML
-    private ListView<?> dashboardListView;
+    private TableColumn<Sale, String> columnDate;   
+    @FXML
+    private TableColumn<Sale, String> columnPropertyType;   
+    @FXML
+    private TableColumn<Sale, Double> columnPrice;   
+    @FXML
+    private TableColumn<Sale, String> columnSeller;   
+    @FXML
+    private TableColumn<Sale, String> columnBuyer;   
+
+    @FXML
+    private TableView<Sale> mainTableView;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        App.dataHandler.getStatitics(dashboardBarChart);
+        App.dataHandler.getStatitics(dashboardBarChart);//provides statistics data to BarChart
+        
+        
+        columnDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        columnPropertyType.setCellValueFactory(new PropertyValueFactory<>("propertyType"));
+        columnPrice.setCellValueFactory(new PropertyValueFactory<>("soldPrice"));
+        columnSeller.setCellValueFactory(new PropertyValueFactory<>("sellerName"));
+        columnBuyer.setCellValueFactory(new PropertyValueFactory<>("buyerName"));
+        
+       
+        
+        ObservableList<Sale> salesObservableList = FXCollections.observableArrayList();
+        salesObservableList.addAll(App.dataHandler.sales.getArrayList());
+        
+        mainTableView.setItems(salesObservableList);
+
     }    
     
 }
