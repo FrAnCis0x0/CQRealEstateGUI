47e5d43823b47fd1cb2b028d91b3aef4af9b5c44
Francis <francis.renzaho@cqumail.com>
2023-05-21 22:30:59 +1000

    added back button function

--- src/main/java/cqrealestatepackage/controller/LandandhouseController.java
+++ src/main/java/cqrealestatepackage/controller/LandandhouseController.java
@@ -5,9 +5,9 @@
 package cqrealestatepackage.controller;
 
 import cqrealestatepackage.App;
-import cqrealestatepackage.model.HouseAndLand;
+import cqrealestatepackage.model.BorderPaneInfo;
 import cqrealestatepackage.model.InputFieldHandler;
-import cqrealestatepackage.model.Land;
+import cqrealestatepackage.model.NavigateToScene;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -47,23 +48,26 @@ public class LandandhouseController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        inputHandler = new InputFieldHandler();
+        inputHandler = new InputFieldHandler();//holds function for validating inputs
         inputHandler.addAllTextFields(tfAddress, tfLotNumber, tfLandArea, tfConstructionArea, tfBedrooms, tfToilets); //add all text fields to inputhandler
-
+        inputHandler.addListenerOnFocus();//add listener to all inputfields
+        //specify which input must be numbers
+        inputHandler.addIntegerListenerOnFocus(tfLotNumber);
+        inputHandler.addIntegerListenerOnFocus(tfLandArea);
+        inputHandler.addIntegerListenerOnFocus(tfConstructionArea);
+        inputHandler.addIntegerListenerOnFocus(tfBedrooms);
+        inputHandler.addIntegerListenerOnFocus(tfToilets);
     }    
 
     @FXML
     private void save(ActionEvent event) {
          //check if all textfields have valid data
         if(inputHandler.textFieldsHaveValue() && inputHandler.tfMustBeInteger(tfLotNumber, tfLandArea, tfConstructionArea, tfBedrooms, tfToilets)){
-           App.dataHandler.AddNewHouseAndLand(tfLotNumber, tfLandArea, tfAddress, tfConstructionArea, tfBedrooms, tfToilets); //save data to file
+            App.dataHandler.AddNewHouseAndLand(tfLotNumber, tfLandArea, tfAddress, tfConstructionArea, tfBedrooms, tfToilets); //save data to file
+            inputHandler.clear(); // clear data in text fields
         }else{
-           
             inputHandler.showEmptyTextField(); // change borders to red
-          
                 
-                //TODO - Show tooltip
-            
         }
     }
 
@@ -72,6 +76,22 @@ public class LandandhouseController implements Initializable {
         inputHandler.clear(); // clear data in text fields
 
         
+    }
+    
+    
+    //Back button uses this to go back to property menu
+    @FXML
+    private void gotoPropertyMenu(ActionEvent event){
+        NavigateToScene navToScene = new NavigateToScene();
+        try{
+            //when goto back to main property menu
+            Pane view = navToScene.getFxml("addProperty");
+            BorderPaneInfo.borderPane.setCenter(view);
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
 }  
